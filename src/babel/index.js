f63926085b49d1b40c8c3f1873ae441d1516daa8
srbberlin <info@hans-schrieber.de>
2019-02-27 13:34:18 +0100

    Initial version.

---
+++ src/babel/index.js
@@ -0,0 +1,35 @@
+window.onload = function () {
+    let sections = document.getElementsByClassName('sects')
+    let backGrounds = [
+        "url('assets/abstract-1501710_1280.jpg')",
+        "url('assets/business-1012761_1920.jpg')",
+        "url('assets/matrix-1735640_1920.jpg')",
+        "url('assets/social-networking-2187996_1920.jpg')"
+    ]
+    let url = '#header'
+
+    function resize () {
+        var height = window.innerHeight + 'px'
+        for (var i = 0; i < sections.length; i++) {
+            sections[i].style.height = height
+        }
+        window.location.href = url;
+    }
+    
+    function changeUrl (e) {
+        url = e.target.hash
+    }
+    
+    for (var i = 0; i < sections.length; i++) {
+        let section = sections[i]
+        section.onclick = changeUrl
+        section.style.backgroundImage = backGrounds[i]
+        section.style.backgroundSize="cover"
+        section.style.backgroundPosition="center"
+    }
+
+    resize()
+
+    window.onresize = resize
+}
+
